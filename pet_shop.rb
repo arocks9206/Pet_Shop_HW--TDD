492c9291d9d4c5604940ae396dff304dafc60054
Alexandria Rocks <alexandria.m.rocks@gmail.com>
2020-03-14 19:26:32 +0000

    Pet_Shop Tests 11-13

--- pet_shop.rb
+++ pet_shop.rb
@@ -46,9 +46,13 @@ def find_pet_by_name(pet_shop, name)
 end
 
 def remove_pet_by_name(pet_shop, name)
-  for pet in @pet_shop[:pets]
-    if pet[:name] == name
-      return pet.delete
+  for pet_name in @pet_shop[:pets]
+    if pet_name[:name] == name
+      return pet_shop[:pets].delete(pet_name)
     end
   end
-end 
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  return @pet_shop[:pets].push (new_pet)
+end
